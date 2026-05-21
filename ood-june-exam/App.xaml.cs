79e09d023b00e4594a3ed7c071f84acd5912470a
Mathieustutz-STUDENT <Mathieustutz-STUDENT@S-5N1H504-SL30>
2026-05-21 13:06:10 +0100

    Add project files.

---
+++ ood-june-exam/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ood_june_exam
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
